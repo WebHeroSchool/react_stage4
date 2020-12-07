6d40f2ed1419433aa28addc1190756259d7da8f5
Ариадна Тузина <tz_ardn@mail.ru>
2020-12-08 02:00:41 +0700

    react-element

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const value = (
+  <div>
+    <p>
+      - Hello! How are you?
+    </p>
+    <p>
+      - I`m fine! Thanks.
+    </p>
+
+  </div>
+);
+
+export { value };
---
+++ index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { value } from './App';
+
+ReactDOM.render(
+  value,
+  document.getElementById('root')
+);
